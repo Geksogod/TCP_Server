a6419e9558641600941b51bfd075f409aedaf5b2
Evgeniy Siecho <evgeniy.siecho@logika.com.ua>
2020-07-03 00:23:44 +0300

    firstCommit

---
+++ TCP_Server/Server/TCP_Server.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Net;
+using System.Net.Sockets;
+
+namespace TCP_Server.Server
+{
+    class TCP_Server
+    {
+        public void StartServer(string ip,string port)
+        {
+
+        }
+
+
+    }
+}
